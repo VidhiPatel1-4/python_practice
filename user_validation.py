155083f61a2f407972d8993a8b43eaa531572362
VidhiPatel1-4 <vidhipatel1498@gmail.com>
2024-07-02 18:37:09 +0530

    first commit

---
+++ user_validation.py
@@ -0,0 +1,21 @@
+
+# User Validation
+
+f_name = (input("Enter First Name:"))
+
+
+if (f_name.isalpha()) and (f_name!=0) and (2<=len(f_name)<= 10):
+    print("First Name:", f_name)
+else:
+    print("Enter Correct Name")
+    print(input("Enter First Name:"))
+
+
+
+
+
+
+
+
+
+
