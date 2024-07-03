69cc53ef8296cec508a93d823f7b6f14ec1bc510
Vidhi Patel <85873227+VidhiPatel1-4@users.noreply.github.com>
2024-07-03 18:23:14 +0530

    Update find_greater_number.py

--- find_greater_number.py
+++ find_greater_number.py
@@ -9,7 +9,7 @@ if num1 > num2 and num1 > num3:
 elif num2 > num3 and num2 > num1:
     print(num2, "Num2 is greater")
 
-elif num1 == num2 or num2 == num3 or num3 == num1:
+elif num1 == num2 and num2 == num3 and num3 == num1:
     print("Any Two or Three Numbers are same")
 else:
     print(num3, "Num3 is greater")
