9da3cb719edaa0fd591a0b54dc6d2c97cd507aab
Vidhi Patel <85873227+VidhiPatel1-4@users.noreply.github.com>
2024-07-10 23:56:04 +0530

    Update user_validation.py

--- user_validation.py
+++ user_validation.py
@@ -1,16 +1,17 @@
-
 # User Validation
 
-f_name = (input("Enter First Name:"))
-
-
-if (f_name.isalpha()) and (f_name!=0) and (2<=len(f_name)<= 10):
-    print("First Name:", f_name)
+first_name=input("Enter First Name: ")
+if first_name.isalpha():
+    if len(first_name)>=3 and len(first_name)<=10:
+        print("First Name:", first_name)
+    else:
+        print("invalid name")
+elif first_name.isalnum():
+    print("Number are not allow")
+elif first_name.isalpha() == False or first_name.isalnum() == False:
+    print("NNot valid")
 else:
-    print("Enter Correct Name")
-    print(input("Enter First Name:"))
-
-
+    print("not valid")
 
 
 
