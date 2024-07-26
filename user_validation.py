e3caf54bee1997f3c843d85ad9534193e39e8baa
Vidhi Patel <85873227+VidhiPatel1-4@users.noreply.github.com>
2024-07-26 23:40:05 +0530

    Update user_validation.py

--- user_validation.py
+++ user_validation.py
@@ -7,17 +7,18 @@ while True:
     # Define valid characters for letters and spaces
     valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
     # Initialize validation without using a separate boolean variable
     for char in first_name:
         # Check if character is a number
         if len(first_name) < 3 or len(first_name) > 10:
             print("Error: First name not valid.")
             break
-        if char>='0' and char<='9':
+        elif char>='0' and char<='9':
             print("Error: First name contains numbers.")
             break
         elif char == " ":
-            print("Error: First name contains space.")
+            print("Error: First name contains spaces.")
             break
         # Check if character is a special character
         elif char not in valid_chars:
@@ -29,10 +30,24 @@ while True:
         print("First Name:", first_name)
         break
 
+while True:
+    phone_number = input("Enter Phone Number :")
 
+    valid_number = "0,1,2,3,4,5,6,7,8,9"
 
+    for num in phone_number:
+        if num not in valid_number:
+            print("Error: phone number contains other value")
+            break
 
+        elif len(phone_number)<10 or len(phone_number)>10:
+            print("Error: Phone number contains invalid digit")
+            break
 
+        elif num == " ":
+            print("Error: Phone number contains space")
+            break
 
-
-
+    else:
+        print("Phone Number: ", phone_number)
+        break
