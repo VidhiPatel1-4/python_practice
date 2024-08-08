bf3a05fc50121967e74f8f1ff4ae6fee62851ee9
VidhiPatel1-4 <vidhipatel1498@gmail.com>
2024-08-08 12:18:55 +0530

    Your commit message

--- find_greater_number.py
+++ find_greater_number.py
@@ -3,15 +3,30 @@ num1 = int(input("Enter First Number:"))
 num2 = int(input("Enter Second Number:"))
 num3 = int(input("Enter Third Number:"))
 
-if num1 > num2 and num1 > num3:
-    print(num1, "Num1 is greater")
+if num1 >= num2 and num1 >= num3:
+    print(num1, "is greater")
 
-elif num2 > num3 and num2 > num1:
-    print(num2, "Num2 is greater")
+elif num2 >= num3 and num2 >= num1:
+    print(num2, "is greater")
 
-elif num1 == num2 or num2 == num3 or num3 == num1:
-    print("Any Two or Three Numbers are same")
 else:
-    print(num3, "Num3 is greater")
+    print(num3, "is greater")
 
+
+print()
+num1 = float(input("Enter First Number:"))
+num2 = float(input("Enter Second Number:"))
+num3 = float(input("Enter Third Number:"))
+
+if num1 > num2:
+    if num1 > num3:
+        print(num1, "is greater")
+    else:
+        print(num3, "is greater")
+
+elif num2 > num3:
+    print(num2, "is greater")
+
+else:
+    print(num3, "is greater")
 
