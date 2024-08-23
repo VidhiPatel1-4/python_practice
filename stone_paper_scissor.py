a781a5118a9b1cbd69c10f9daf7ab617150c3269
VidhiPatel1-4 <vidhipatel1498@gmail.com>
2024-08-23 12:57:04 +0530

    stone paper scissor game

--- stone_paper_scissor.py
+++ stone_paper_scissor.py
@@ -1,11 +1,12 @@
-print()
+import random
 
 stone = 1
 paper = 2
 scissor = 3
 
 user_input = int(input("Enter a number from User  (1 = Stone, 2 = Paper, 3 = Scissor): "))
-computer_input = int(input("Enter a number from computer  (1 = Stone, 2 = Paper, 3 = Scissor): "))
+computer_input = random.randint(1, 3)
+print("Enter a number from computer  (1 = Stone, 2 = Paper, 3 = Scissor):", computer_input)
 
 if user_input not in [stone, paper, scissor] or computer_input not in [stone, paper, scissor]:
     print("Invalid number")
